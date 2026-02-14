8a32c22c447801410935159223c4398225118b69
davidacerodev <davidacero.dev@gmail.com>
2026-02-14 10:18:35 -0500

    ubicación

--- front/js/login.js
+++ front/js/login.js
@@ -5,7 +5,6 @@ function log(){
         {user: "user", pass: "userdoc", rol: "userdoc"},
         {user: "user", pass: "userest", rol: "userest"}
     ];
-//gola
 
     const user=document.getElementById('user').value;
     const pass=document.getElementById('password').value;
